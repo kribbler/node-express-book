197554db2e8fda0850e21f007cc2e08234d5ed91
Daniel Oraca <daniel.oraca@gmail.com>
2018-03-09 10:56:50 +0000

    first step

---
+++ meadowlark.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+//setup handlebars
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+];
+
+app.get('/', function(req, res) {
+    res.type('text/html');
+    res.render('home');
+});
+
+app.get('/about', function(req, res) {
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {
+        fortune: randomFortune
+    });
+})
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next) {
+    res.status(500);
+    res.render('500');
+});
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' + app.get('port'));
+})
+
